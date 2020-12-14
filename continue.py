0ab5a90545373c50ecaf69cf10623084f3808ddf
ftraymax <75973415+ftraymax@users.noreply.github.com>
2020-12-14 18:56:05 +0530

    Add files via upload

---
+++ continue.py
@@ -0,0 +1,10 @@
+while True:
+    print('enter your name')
+    name = input()
+    if name != 'ram':
+        #continue
+        print('hello there, whats your password')
+        password = input()
+        if password == 'hola':
+            break
+            print('thank you')
